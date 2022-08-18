1996f19e5f0a02c722552cbc74da7279b566dbf2
Đoàn Quốc Việt <viethy01@gmail.com>
2022-08-18 16:54:59 +0700

    đăng nhập và quên mật khẩu
    đã hoàn thành giao diện và quên mật khẩu

---
+++ app/src/main/java/com/utehy/econtact/econtact/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.utehy.econtact.econtact;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.utehy.econtact.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/utehy/econtact/econtact/MainActivity.java
+++ app/src/main/java/com/utehy/econtact/econtact/MainActivity.java
@@ -1,14 +1,16 @@
-package com.utehy.econtact;
+package com.utehy.econtact.econtact;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.utehy.econtact.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
     // tinh dien tich hcn
     public void mangphu(int i){
