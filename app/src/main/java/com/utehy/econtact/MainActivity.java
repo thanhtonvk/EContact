3d13efc22e7fcaa16e81f6ee3eb84c050ad2cfdf
datnguyen2507 <dogvscat2507@gmail.com>
2022-08-19 14:59:02 +0700

    giao diện (trang chủ, hồ sơ, quá trình)
    - trang chủ, hồ sơ đã hoàn thiện
    - quá trình và các chức năng khác đang trong quá trình code

--- app/src/main/java/com/utehy/econtact/MainActivity.java
+++ app/src/main/java/com/utehy/econtact/MainActivity.java
@@ -13,14 +13,12 @@ public class MainActivity extends AppCompatActivity {
     // tinh dien tich hcn
     public void mangphu(int i){
         for (int j = i+1; j < 50; j++) {
-
         }
     }
+    //đây là khi thay đổi code
     public void mang(){
         for (int i = 0; i < 5; i++) {
               mangphu(i);
-//đây là khi thay đổi code
         }
     }
 }
-//lol
