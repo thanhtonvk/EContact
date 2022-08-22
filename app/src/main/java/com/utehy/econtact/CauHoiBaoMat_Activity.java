1f88d08c24cbc4328c2a9324ef941e5f7c0f3df7
Đoàn Quốc Việt <viethy01@gmail.com>
2022-08-22 16:52:44 +0700

    làm giao diện quên mật khẩu và câu hỏi bảo mật
    đã hoàn thành

--- app/src/main/java/com/utehy/econtact/CauHoiBaoMat_Activity.java
+++ app/src/main/java/com/utehy/econtact/CauHoiBaoMat_Activity.java
@@ -1,14 +1,13 @@
 package com.utehy.econtact;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class QuenmatkhauActivity extends AppCompatActivity {
+public class CauHoiBaoMat_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hien_matkhau);
+        setContentView(R.layout.activity_cau_hoi_bao_mat);
     }
 }
--- app/src/main/java/com/utehy/econtact/econtact/QuenmatkhauActivity.java
+++ app/src/main/java/com/utehy/econtact/econtact/QuenmatkhauActivity.java
@@ -11,6 +11,6 @@ public class QuenmatkhauActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quenmatkhau);
+        setContentView(R.layout.activity_hien_matkhau);
     }
 }
