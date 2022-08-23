a243aba34334ccbda68f0ad7d29f673aab37f2f9
tophong212 <tophong212@gmail.com>
2022-08-23 16:53:26 +0700

    Giao diện đăng nhập

--- app/src/main/java/com/utehy/econtact/LoginActivity.java
+++ app/src/main/java/com/utehy/econtact/LoginActivity.java
@@ -3,12 +3,16 @@ package com.utehy.econtact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
