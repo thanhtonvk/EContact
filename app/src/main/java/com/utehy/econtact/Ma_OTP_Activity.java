1e8e7e5a0be5dbe5de1b47cc3397f67ea5ee1125
Đoàn Quốc Việt <viethy01@gmail.com>
2022-08-25 15:19:12 +0700

    làm giao diện OTP
    hoàn thành giao diên OTP

---
+++ app/src/main/java/com/utehy/econtact/Ma_OTP_Activity.java
@@ -0,0 +1,14 @@
+package com.utehy.econtact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ma_OTP_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ma_otp);
+    }
+}
