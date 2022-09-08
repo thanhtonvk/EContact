41fc962b5b4ecf123c78c4b5a82c923d21b0d019
Đoàn Quốc Việt <viethy01@gmail.com>
2022-09-08 10:48:20 +0700

    làm lại giao diện và code mã OTP
    Đã làm lại giao diện và code mã OTP

--- app/src/main/java/com/utehy/econtact/Activity/Ma_OTP_Activity.java
+++ app/src/main/java/com/utehy/econtact/Activity/Ma_OTP_Activity.java
@@ -6,20 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.utehy.econtact.R;
 
 public class Ma_OTP_Activity extends AppCompatActivity {
 
+    private EditText edtOTP;
+    private Button btnSendOTP;
+    private TextView tvSendOTPAgain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ma_otp);
 
-        Button btn_gui_OTP = findViewById(R.id.btn_gui_OTP);
+        Button btn_send_OTP_code = findViewById(R.id.btn_send_OTP_code);
 
-        btn_gui_OTP.setOnClickListener(new View.OnClickListener() {
+        btn_send_OTP_code.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), QuenmatkhauActivity.class);
@@ -27,5 +33,40 @@ public class Ma_OTP_Activity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        setTitleToolbar();
+
+        Anhxa();
+        btnSendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strOTP = edtOTP.getText().toString().trim();
+                onClickSendOTPCode(strOTP);
+            }
+        });
+        tvSendOTPAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClicktvSendOTPAgain();
+            }
+        });
+    }
+    private  void setTitleToolbar(){
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Enter_OTP");
+        }
+    }
+
+    private void Anhxa(){
+        edtOTP = findViewById(R.id.edt_OTP);
+        btnSendOTP = findViewById(R.id.btn_send_OTP_code);
+        tvSendOTPAgain = findViewById(R.id.tv_send_OTP_again);
+    }
+
+    private void onClickSendOTPCode(String strOTP) {
+
+    }
+
+    private void onClicktvSendOTPAgain(){
+
     }
 }
