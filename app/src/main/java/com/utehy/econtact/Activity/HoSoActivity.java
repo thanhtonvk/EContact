cb0abbbf2f38e422a05a286f26b520c3e40843f1
LeHuong02 <101835537+LeHuong02@users.noreply.github.com>
2022-09-09 09:25:42 +0700

    code hồ sơ, responsemessage

--- app/src/main/java/com/utehy/econtact/Activity/HoSoActivity.java
+++ app/src/main/java/com/utehy/econtact/Activity/HoSoActivity.java
@@ -11,13 +11,29 @@ import com.utehy.econtact.R;
 
 public class HoSoActivity extends AppCompatActivity {
     ImageButton img_trolai;
-    ImageView img_anhhoso;
-    TextView tv1,tv2,tv3;
+    ImageView anhhoso;
+    TextView tv_hoso_hoten,tv_hoso_ngaysinh, tv_hoso_diachi,tv_hoso_lop, tv_hoso_masinhvien, tv_hoso_gvcn,tv_hoso_gioitinh,tv_hoso_hanhkiem,tv_hoso_hocluc;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoso_sinhvien);
+        anhxa();
+
     }
+    private void anhxa(){
+        img_trolai = findViewById(R.id.img_trolai);
+        anhhoso = findViewById(R.id.anhhoso);
+        tv_hoso_hoten = findViewById(R.id.tv_hoso_hoten);
+        tv_hoso_ngaysinh = findViewById(R.id.tv_hoso_ngaysinh);
+        tv_hoso_diachi = findViewById(R.id.tv_hoso_diachi);
+        tv_hoso_lop = findViewById(R.id.tv_hoso_lop);
+        tv_hoso_masinhvien = findViewById(R.id.tv_hoso_masinhvien);
+        tv_hoso_gvcn = findViewById(R.id.tv_hoso_gvcn);
+        tv_hoso_gioitinh = findViewById(R.id.tv_hoso_gioitinh);
+        tv_hoso_hanhkiem = findViewById(R.id.tv_hoso_hanhkiem);
+        tv_hoso_hocluc = findViewById(R.id.tv_hoso_hocluc);
+    }
+
 }
